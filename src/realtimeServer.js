a95b0a3c2b48620d1335e5159c1824463e876cfa
Danniel Navas <dannielnavas@gmail.com>
2025-03-07 07:20:17 -0500

    ✨ Mejorar la funcionalidad de chat: limpiar el campo de entrada y establecer el foco después de enviar un mensaje; incluir el nombre de usuario desde las cookies en los mensajes emitidos.

--- src/realtimeServer.js
+++ src/realtimeServer.js
@@ -2,9 +2,12 @@ module.exports = (httpServer) => {
   const { Server } = require("socket.io");
   const io = new Server(httpServer);
   io.on("connection", (socket) => {
+    const cookies = socket.handshake.headers.cookie;
+    const user = cookies.split("=").pop();
+
     socket.on("message", (message) => {
       io.emit("message", {
-        user: "Anonymous",
+        user,
         message,
       });
     });
